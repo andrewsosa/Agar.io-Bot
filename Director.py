26bcb7433f89e1ca943f2f8ae6b2b94909999b6b
Andrew Sosa <sosa@cs.fsu.edu>
2015-06-22 14:42:00 -0400

    Initial push

---
+++ Director.py
@@ -0,0 +1,23 @@
+import cv2
+import cv2.cv as cv
+import numpy as np
+
+img = cv2.imread('pic1.png',0)
+#ret, img = cv2.threshold(img,210,255,cv2.THRESH_BINARY)
+
+
+
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+
+circles = cv2.HoughCircles(img, cv.CV_HOUGH_GRADIENT, 50, 1, param1=50, param2=30, minRadius=0, maxRadius=0)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+cv2.imshow('detected circles',cimg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
